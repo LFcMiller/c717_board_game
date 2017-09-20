f127d7386be677613c0c79a1ef6eff7649eaed4a
Matt Geller <MattG92@gmail.com>
2017-09-19 19:49:55 -0700

    Tim helped me write some monster SQL

--- 3plus_table_solution/player_data_api/retrieve_past_activity.php
+++ 3plus_table_solution/player_data_api/retrieve_past_activity.php
@@ -1,10 +1,25 @@
 <?php
 
-if(empty($_POST['user_ID'])){
-    $output['errors'][] = 'I need to know who to search for! (Hint: make sure the value is \'user_ID\'';
+if(empty($_POST['fb_ID'])){
+    $output['errors'][] = 'I need to know who to search for! (Hint: make sure the value is \'fb_ID\'';
     die();
 }
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
-$query = "SELECT `game_name` FROM ";//not done!
+//thanks, Tim!
+$query =
+    "
+SELECT e.`game_name` AS game_name, COUNT(e.`event_ID`) AS frequency
+
+	FROM `users` AS u
+	JOIN `users_to_events` AS ue ON u.`user_ID`=ue.`player_ID`
+	JOIN `events` AS e ON ue.`event_ID`=e.`event_ID`
+
+	WHERE u.`fb_ID`={$_POST['fb_ID']}
+	AND ue.`role` IN ('host','attendee')
+	AND e.`date`<CURDATE()
+
+	GROUP BY game_name
+;
+    ";
