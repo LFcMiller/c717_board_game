df469c22604834eb9f1236c448b3c418e7aa75d4
Cody Miller <miskatonicraft@gmail.com>
2017-09-23 11:36:27 -0700

    Fixed Event Posting - Cody

--- createevent.js
+++ createevent.js
@@ -1,6 +1,39 @@
 
 function sendData(){
   event.preventDefault();
+  var address = $("#address").val()+", "+$("#city").val()+", "+$("#state").val()
+  getLatLong(address).then(getCrossStreets).then(handleSuccess);
+}
+
+function getLatLong(address) {
+  var deferred = $.Deferred();
+  $.ajax({
+    url: 'https://maps.googleapis.com/maps/api/geocode/json',
+    method: "GET",
+    data: {
+      address: address,
+      key: 'AIzaSyDxsAjsSwsaBzaz-xNaLnDUQEjr_BIsiCE'
+    },
+    success: deferred.resolve
+  })
+  return deferred;
+}
+function getCrossStreets(response) {
+  console.log(response.results[0].geometry.location);
+  var deferred = $.Deferred();
+  $.ajax({
+    url: 'https://roads.googleapis.com/v1/nearestRoads',
+    method: "GET",
+    data: {
+      points: `${response.results[0].geometry.location.lat}, ${response.results[0].geometry.location.lng}`,
+      key: 'AIzaSyDxsAjsSwsaBzaz-xNaLnDUQEjr_BIsiCE'
+    },
+    success: deferred.resolve
+  })
+  return deferred;
+}
+function handleSuccess(response) {
+  console.log(response);
   var result = {
     game_name: $("#gameName").val(),
     num_players: $("#numPlayers").val(),
@@ -10,11 +43,22 @@ function sendData(){
     zip: $("#zipcode").val(),
     general_details: $("#textDetails").val(),
     date: $("#year").val()+"-"+$("#month").val()+"-"+$("#day").val(),
-    time: $("#time").val(),
-    dayNight: $("#dayNight").val()
+    time: $("#time").val()+" "+$("#dayNight").val(),
+    lat: response.snappedPoints[0].location.latitude,
+    lng: response.snappedPoints[0].location.longitude,
   };
   console.log(result);
-  // $.ajax({
-  //   url: 
-  // })
+  $.ajax({
+    method: 'post',
+    dataType: 'json',
+    url: "./3plus_table_solution/event_input_decision_maker.php?action=newEvent",
+    data: result,
+    timeout: 5000,
+    success: function (objectFromServer) {
+        console.log(objectFromServer);
+    },
+    error: function (xhr, textStatus, errorString) {
+        console.log(errorString);
+    }
+  });
 }
