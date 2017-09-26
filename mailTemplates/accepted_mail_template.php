d95d5d9b94f8832c7cd561da65749196fafdd66e
Austin Violette <violette.austin@yahoo.com>
2017-09-25 19:56:16 -0700

    new php mailer and email template

---
+++ mailTemplates/accepted_mail_template.php
@@ -0,0 +1,7 @@
+<?php
+$accepted_body = <<<ENDDATA
+	<div>Hello, You have been invited to ... BoardGame! please Accept or Decline this invitation</div>
+	<button>Accept</button>
+	<button>Decline</button>
+ENDDATA;
+?>
---
+++ mailTemplates/confirmation_mail_template.php
@@ -0,0 +1,6 @@
+<?php
+$confirmation_body = <<<ENDDATA
+	<h1>The user has Accepted and Confirmed your invitation</h1>
+	<div>We have sent them the location of the event and your contact email.</div>
+ENDDATA;
+?>
