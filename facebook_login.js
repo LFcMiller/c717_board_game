f5ae1be95400a620a694849096a4168e8e3fdd62
Austin Violette <violette.austin@yahoo.com>
2017-09-27 18:42:00 -0700

    latest fb changes - austin

--- facebook_login.js
+++ facebook_login.js
@@ -1,24 +1,20 @@
-var facebook_id = '';
-var facebook_name = '';
-var facebook_email = '';
-
 
 function sendFacebookData(response){
-  console.log('sendFacebookData test');
+  console.log('sendFacebookData');
   $.ajax({
       url: './3plus_table_solution/player_input_decision_maker.php?action=relateOrCreateUser',
       method: "POST",
       dataType: "json",
       data: {
-        fb_ID: facebook_id,
-        name: facebook_name,
-        email: facebook_email
+        fb_ID: response.id,
+        name: response.name,
+        email: response.email
       },
       success: function(response){
         console.log('success',response);
       },
       error: function(response){
-        console.log('error',response);
+        console.log('errorrrrrrrrrrrrrrrr',response);
       }
   });
 }
