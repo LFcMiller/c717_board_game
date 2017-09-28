e530c710e9b02508d8489c6af7a1b67387eb6161
Austin Violette <violette.austin@yahoo.com>
2017-09-27 19:14:03 -0700

    fixed FB login

--- 3plus_table_solution/player_data_api/create_new_user.php
+++ 3plus_table_solution/player_data_api/create_new_user.php
@@ -8,15 +8,19 @@
 if (empty($_POST['first_name']) || empty($_POST['email']) || empty($_POST['fb_ID'])){
     //TODO: make this a little bit cleaner
     $output['errors'][] = 'Missing input:';
-    if (empty($first_name))
+    if (empty($_POST['first_name']))
         $output['errors'][] = 'first name';
-    if (empty($email))
+    if (empty($_POST['email']))
         $output['errors'][] = 'email';
-    if (empty($fb_ID))
+    if (empty($_POST['fb_ID']))
         $output['errors'][] = 'facebook ID';
     return;
     $output['errors'][] = 'But it\'s ok I\'ll try to add your entry to the database anyway....for now';
 }
+
+$first_name = $_POST['first_name'];
+$email = $_POST['email'];
+$fb_ID = $_POST['fb_ID'];
 
 $new_person_query = "INSERT INTO `users` SET `first_name` = '$first_name', `email`='$email', `fb_ID`='$fb_ID'";
 //print($new_person_query);
