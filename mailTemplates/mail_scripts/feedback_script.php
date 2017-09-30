0d3d4bd7b20c00b69eee54c04f56888b0585a4c3
Matt Geller <MattG92@gmail.com>
2017-09-30 16:43:55 -0700

    made the feedback_script.php

---
+++ mailTemplates/mail_scripts/feedback_script.php
@@ -0,0 +1,21 @@
+<?php
+
+$output['debugging_messages'][]='started feedback_script';
+
+if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['message'])){
+    $output['errors'][] = 'Missing input';
+    if(empty($_POST['name']))
+        $output['errors'][] = 'name';
+    if(empty($_POST['email']))
+        $output['errors'][] = 'email';
+    if(empty($_POST['message']))
+        $output['errors'][] = 'message';
+    return;
+}
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+$output['debugging_messages'][]='variables needed for the feedback email should be set now.';
+$output['success'] = true;
