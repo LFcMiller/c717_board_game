31a488c02c4a752a687785fe98e20f6fbad797e3
soyjoysauce <soyjoysauce@gmail.com>
2017-10-02 16:31:42 -0700

    Soy added some styling to nearby game and optimized mobile version

--- nearbyGames.php
+++ nearbyGames.php
@@ -14,7 +14,7 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
   <title>Nearby Games</title>
   <link rel="stylesheet" href="style.css">
-
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <script src="facebook_login.js"></script>
 
     <?php
@@ -39,7 +39,7 @@
   <script src="nearbyGames.js"></script>
 
 </head>
-<body class="body_black">
+<body class="container-fluid padding_unset body_black">
         <script>
         window.fbAsyncInit = function() {
             FB.init({
@@ -91,10 +91,9 @@
         }(document, 'script', 'facebook-jssdk'));
       </script>
     <div class="create_event_background"></div>
-    <div class="container-fluid padding_unset">
-        <div class="col-md-12">
+    <div class="row padding_unset">
             <!--header starts here-->
-            <div class="margin_bottom margin_top col-sm-12 col-md-12">
+            <div class="margin_bottom col-sm-12 col-md-12">
                 <div class="topnav margin_bottom col-sm-12 col-md-12" id="myTopnav">
                     <a href="/" id="home" class="col-sm-3 col-md-3">Home</a>
                     <a href="/about" id="about"class="col-sm-3 col-md-3">About</a>
@@ -102,54 +101,59 @@
                     <a href="/contact_us.html" id="contact" class="col-sm-3 col-md-3">Contact</a>
                 </div>
                 <!--title and logo-->
-                <div class="col-md-12">
+                <div class="col-sm-12 col-md-12">
                     <div class="hidden-xs hidden-sm visible-md visible-lg col-md-2">
                         <img class="temp_logo display-inline" src="./imgs/temp logo.png"/>
                     </div>
                     <!--mobile title-->
                     <div class="margin_bottom visible-xs visible-sm hidden-md col-sm-12">
                         <h3 class="font_lg">Board Game Scout</h3>
-                        <h4 class ="font_md">Meet-up App for Fellow Boarders Near You!</h4>
+                        <h3 class ="font_rg">Scroll Down for Game Map and Apply!</h3>
                     </div>
                     <!--desktop title-->
                     <div class="hidden-sm visible-md visible-lg col-md-8">
                         <h1 class="font_lg">Board Game Scout</h1>
                     </div>
                     <!--button_container-->
-                    <div class="button_container col-sm-12 col-md-4 col-md-offset-8">
+                    <div class="button_container col-sm-12 col-md-11">
                         <!--facebook_login_button-->
-                        <div class="col-sm-12 col-md-6 col-md-offset-3">
-                            <fb:login-button class="fb-login-button col-lg-2" data-max-rows="1" data-size="large" data-button-type="continue_with" data-show-faces="false" data-auto-logout-link="true" data-use-continue-as="false" scope="public_profile,email,picture" onlogin="checkLoginState();">
-                        </fb:login-button>
-                </div>
-            </div>
-            <!--map and info column for DESK-TOP-->
-            <div class="info-and-map col-sm-12 col-md-12">
-                <div class="info-column-div col-sm-12 col-md-2">
-                    <div class="gamesContainer column_height col-sm-12 col-md-12">
-                        <h1 class="text-center ">Games Near You:</h1>
-                        <h2>Sorry, there are currently no games in your area.</h2>
-                        <!-- <div class="game col-xs-12">
-                            <div class="gameName truncate col-xs-12" index="0">Monopoly</div>
-                        </div> -->
-                    </div>
-                </div>
-                    <div class="col-sm-12 col-md-8 " id="map"></div>
-                <div class="info-column-div col-sm-12 col-md-2">
-                    <div class="info-column-div column_height col-sm-12 col-md-12">
-                        <h1 class="text-center ">Games Info:</h1>
-                        <div class="eventInfo">
-                            <!-- <div class="date col-xs-12">Date: 2017-10-28</div>
-                            <div class="time col-xs-12">Time: 5:00 PM</div>
-                            <div class="details col-xs-12">Details: experienced players preferred, college students preferred, parking available, food provided</div>
-                            <button class="btn btn-success apply" index="0">Apply</button> -->
+                        <div class="col-sm-12 col-md-2 col-md-offset-10">
+                            <!--this was the previous fb button that was not working. kept for comparison purpose-->
+                            <!--<fb:login-button class="fb-login-button col-md-1" data-max-rows="1" data-size="large" data-button-type="continue_with" data-show-faces="false" data-auto-logout-link="true" data-use-continue-as="false" scope="public_profile,email,picture" onlogin="checkLoginState();">-->
+                            <!--</fb:login-button>-->
+                            <!--this button works!!!!!-->
+                            <fb:login-button class="fb-login-button col-lg-2 fb_iframe_widget" data-max-rows="1" data-size="large" data-button-type="continue_with" data-show-faces="false" data-auto-logout-link="true" data-use-continue-as="false" scope="public_profile,email" onlogin="checkLoginState();" login_text="
+                        " fb-xfbml-state="rendered" fb-iframe-plugin-query="app_id=133764540588384&amp;auto_logout_link=true&amp;button_type=continue_with&amp;container_width=279&amp;locale=en_US&amp;login_text=%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20&amp;max_rows=1&amp;scope=public_profile%2Cemail&amp;sdk=joey&amp;show_faces=false&amp;size=large&amp;use_continue_as=false"><span style="vertical-align: bottom; width: 171px; height: 40px;"><iframe name="f1c8511f428535c" width="1000px" height="1000px" frameborder="0" allowtransparency="true" allowfullscreen="true" scrolling="no" title="fb:login_button Facebook Social Plugin" src="https://www.facebook.com/v2.10/plugins/login_button.php?app_id=133764540588384&amp;auto_logout_link=true&amp;button_type=continue_with&amp;channel=https%3A%2F%2Fstaticxx.facebook.com%2Fconnect%2Fxd_arbiter%2Fr%2FZ2duorNoYeF.js%3Fversion%3D42%23cb%3Df1f0d3420b5872c%26domain%3Ddev.boardgamescout.com%26origin%3Dhttps%253A%252F%252Fdev.boardgamescout.com%252Ff33a7e53affa3fc%26relation%3Dparent.parent&amp;container_width=279&amp;locale=en_US&amp;login_text=%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20&amp;max_rows=1&amp;scope=public_profile%2Cemail&amp;sdk=joey&amp;show_faces=false&amp;size=large&amp;use_continue_as=false" style="border: none; visibility: visible; width: 171px; height: 40px;" class=""></iframe></span></fb:login-button>
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>
+                    <!--map and info column for DESK-TOP-->
+                    <div class="info-and-map col-sm-12 col-md-12">
+                        <div class="info-column-div col-sm-12 col-md-2">
+                            <h2 class="text-center green">Games Near You:</h2>
+                            <div class="gamesContainer column_height col-sm-12 col-md-12">
+                                <h2>Sorry, there are currently no games in your area.</h2>
+                                <!-- <div class="game col-xs-12">
+                                    <div class="gameName truncate col-xs-12" index="0">Monopoly</div>
+                                </div> -->
+                            </div>
+                        </div>
+                        <div class="col-sm-12 col-md-8 " id="map"></div>
+                        <div class="info-column-div col-sm-12 col-md-2">
+                            <div class="info-column-div column_height col-sm-12 col-md-12">
+                                <h1 class="text-center green">Games Info:</h1>
+                                <div class="eventInfo">
+                                    <!-- <div class="date col-xs-12">Date: 2017-10-28</div>
+                                    <div class="time col-xs-12">Time: 5:00 PM</div>
+                                    <div class="details col-xs-12">Details: experienced players preferred, college students preferred, parking available, food provided</div>
+                                    <button class="btn btn-success apply" index="0">Apply</button> -->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
         <!--footer-->
-        <div class="footer row col-md-12">
+        <div class="footer col-md-12">
             <!--paypal_button-->
             <div class="col-sm-12 col-md-12">
                 <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
@@ -159,7 +163,6 @@
                     <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
                     <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
                 </form>
-
             </div>
         </div>
     </div>
