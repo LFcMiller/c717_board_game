356873efe9865fe85353f63e3d591c57e7d0af5d
Matt Geller <MattG92@gmail.com>
2017-10-05 19:21:10 -0700

    touchups to the backend

--- back_end/event_input_decision_maker.php
+++ back_end/event_input_decision_maker.php
@@ -6,13 +6,11 @@
 //(this script and its subsidiaries are inspired by the php_SGTserver prototype.)
 
 if(empty($_GET['action']/* check if the get superglobal variable 'action' is empty*/)){
-    exit('no action specified');
+    $output['errors'][] = 'no action specified';
+    die(json_encode($output));
 }
 
-//require the mysqlConnect.php file. Make sure you configured it properly!
-require_once('mysqlConnect.php');
-
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+require_once('database_connect_script.php');
 
 $output = [
     'success'=> false, //we assume we will fail
@@ -22,9 +20,6 @@ $output = [
 //check the GET superglobal so you know what the front end is asking you to do
 
 switch($_GET['action']){
-    case 'readAll':
-        include_once('event_data_api/send_all_events.php');
-        break;
     case 'readByZip':
         include_once('event_data_api/send_events_by_zip.php');
         break;
@@ -35,11 +30,13 @@ switch($_GET['action']){
         include_once('event_data_api/apply_script.php');
         break;
 
+    //this action can be useful for debugging, but should not be callable for security purposes.
+//    case 'readAll':
+//        include_once('event_data_api/send_all_events.php');
+//        break;
 
     default:
         array_push($output['errors'],'I don\'t even know what action you want the back end to do. (Hint: make sure you specify the \'action\' in the query string of your ajax url.)');
 }
-
-//Make the date nicer for humans
 
 print(json_encode($output));
